15ffb2022e4edde54db6a5cec9d35c119f887247
Eden Toledano <edentoledano@gmail.com>
2020-04-16 22:00:20 +0300

    some clarifications

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import asyncio
 from _thread import start_new_thread
-from flask import Flask
+import time
 
+from flask import Flask
 from ws_server import WebSocketServer
 
 app = Flask(__name__)
 
-app_prefix = '/api'
 _TAG = '[Main]'
 ws_server: WebSocketServer = None
 
+# TypeError: The view function did not return a valid response.
+# The return type must be a string, dict, tuple, Response instance, or WSGI callable, but it was a coroutine.
 @app.route('/broadcast', methods=['GET'])
-def broadcast():
+async def flask_broadcast():
+    message = "some dummy msg"
+    # How can I use this without making flask_broadcast a coroutine?
+    await ws_server.broadcast(message)
     return {"res": "eden"}
 
 # Blocking
@@ -21,15 +27,27 @@ def start_http_server(port: int) -> None:
 # Without 'async': SyntaxError: 'await' outside async function
 async def init_web_socket_as_server(port: int, host: str) -> None:
     global ws_server
-    ws_server = WebSocketServer(port, host)
+    ws_server = WebSocketServer(port, host, on_new_message)
     # Without 'await': RuntimeWarning: coroutine 'WebSocketServer.connect' was never awaited
     await ws_server.connect()
 
-async def init() -> None:
+async def on_new_message(message: str) -> None:
+    # For example, whenever there's a new message, ask the ws_server to broadcast it
+    # await ws_server.broadcast(message)
+    # Do some outside logic
+    pass
+
+def broadcast_sync(message):
+    time.sleep(2)
+
+async def broadcast_async(message):
+    asyncio.sleep(2)
+
+def init() -> None:
     ws_port = 8765
     rest_port = 5000
     start_new_thread(start_http_server, (rest_port,))
-    await init_web_socket_as_server(ws_port, "localhost")
+    # await init_web_socket_as_server(ws_port, "localhost")
 
 if __name__ == '__main__':
     # Error: SyntaxError: 'await' outside function
--- ws_server.py
+++ ws_server.py
@@ -3,17 +3,24 @@ from typing import Set, Callable
 
 import websockets
 from websockets.http import Headers
-from websockets.server import Serve
+from websockets.server import Serve, WebSocketServerProtocol
 
 clients: Set = set()
 
 
 class WebSocketServer:
-    _on_client_connected: Callable = None
+    # Callbacks for outside usage
+    _on_new_message: Callable = None
 
-    def __init__(self, port: int, host: str):
+    def __init__(
+            self,
+            port: int,
+            host: str,
+
+            on_new_message: Callable):
         self.port = port
         self.host = host
+        self._on_new_message = on_new_message
 
     async def connect(self):
         loop = asyncio.get_event_loop()
@@ -28,47 +35,35 @@ class WebSocketServer:
         print("server started", server)
         loop.run_forever()
 
-    async def wait_for_clients(self, ws: websockets.WebSocketServerProtocol, path: str):
-        print("started")
-
+    async def wait_for_clients(self, ws: WebSocketServerProtocol, path: str):
+        print("Started waiting for clients")
         if ws not in clients:
-            # Authenticate
-            print("New! + registering client...")
-            clients.add(ws)
-            self._on_client_connected(ws)
+            authorized: bool = self.auth_user(ws)
             await self.listen_to_client(ws)
-            print("test")
-            # start_new_thread(listen_to_client, (ws,))
-            header = ws.request_headers.get('Authorization')
-            # if header == 'Basic cm9vdDo3NzA3NzA=':
-            #     print("Authenticated. + registering client...")
-            #     clients.add(ws)
-            # else:
-            #     print("- unregistering client...")
-            #     clients.remove(ws)
-        else:
-            pass
-            # print("listening to client...")
-            #
-            # name = await ws.recv()
-            # print(f"client:  {name}")
-            #
-            # greeting = f"Echo {name}!"
-            #
-            # await ws.send(greeting)
-            # print(f"server:  {greeting}")
-        print("ended")
+            if authorized:
+                print("New authorized user!")
+                clients.add(ws)
+                self._on_client_connected(ws)
+            else:
+                # print("New unauthorized client, disconnecting...")
+                clients.remove(ws)
+
+    def auth_user(self, ws: WebSocketServerProtocol) -> bool:
+        header = ws.request_headers.get('Authorization')
+        # Do some check here, return False if user is not authorized
+        return True
 
     # Blocking
-    async def listen_to_client(self, ws: websockets.WebSocketServerProtocol):
+    async def listen_to_client(self, ws: WebSocketServerProtocol):
         # print("listening to client...")
         while True:
             message = await ws.recv()
             self.on_message(ws, message)
 
-    def on_message(self, ws: websockets.WebSocketServerProtocol, msg: str):
+    def on_incoming_message(self, ws: WebSocketServerProtocol, msg: str):
         print(f"Received new message [{msg}]")
-        self.broadcast(msg)
+        # invoke external callback
+        self._on_new_message(msg)
 
     # Cant use this as async definition
     async def broadcast(self, message: str):
@@ -81,6 +76,4 @@ class WebSocketServer:
         auth = headers.get("Authorization")
         print(f"process_request with path {path}, and token: {auth}")
 
-    def set_fn_new_client(self, on_new_client: Callable):
-        self._on_client_connected = on_new_client
 
